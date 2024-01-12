0146ac593662705d8648bf452b03d5ef19d43ce2
Maciek Gorczynski <maciekg81@gmail.com>
2024-01-12 17:09:48 +0100

    refactor: remove nodemon

--- runner.js
+++ runner.js
@@ -1,7 +1,6 @@
 const childProcess = require("child_process");
 const schedule = require("node-schedule");
 
-// At every 29th from 29 through 59. hh:29, hh:59
 schedule.scheduleJob("0/60 * * * *", () => {
     const process = childProcess.spawn("node index.js", {
         shell: true,
